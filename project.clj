1f32f21f155ee6b7250abfed1e08e92b6a07fef6
Frozenlock <frozenlock@gmail.com>
2013-01-18 14:26:11 -0500

    include externs.js in the distributed .jar!

--- project.clj
+++ project.clj
@@ -1,15 +1,13 @@
-(defproject org.clojars.frozenlock/envision-cljs "0.1.0"
+(defproject org.clojars.frozenlock/envision-cljs "0.1.1-SNAPSHOT"
   :description "Wrapper for the Envision.js library (Graphs!)"
   :url "https://github.com/Frozenlock/envision-cljs"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]]
   :plugins [[lein-cljsbuild "0.2.10"]]
-  :cljsbuild {:builds {:main {:source-path "src-cljs"
+  :hooks [leiningen.cljsbuild]
+  :cljsbuild {:builds {:main {:jar true
+                              :source-path "src-cljs"
                               :compiler {:output-to "resources/public/js/cljs.js"
-                                         :optimizations :advanced
-                                         :warnings false
-                                         :pretty-print false
-                                         :externs ["libs/externs.js"]}
-                              :jar true}}})
+                                         :externs ["src-cljs/externs/externs.js"]}}}})
   
