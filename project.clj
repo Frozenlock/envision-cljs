87e5d7d0f0738c6fbb11e24e8ee92900f0cc26d3
Frozenlock <frozenlock@gmail.com>
2013-01-25 14:03:30 -0500

    well I shouldn't have remove the cljsbuild hook after all...

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
-(defproject org.clojars.frozenlock/envision-cljs "0.1.2"
+(defproject org.clojars.frozenlock/envision-cljs "0.1.3"
   :description "Wrapper for the Envision.js library (Graphs!)"
   :url "https://github.com/Frozenlock/envision-cljs"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]]
   :plugins [[lein-cljsbuild "0.2.10"]]
+  :hooks [leiningen.cljsbuild]
   :cljsbuild {:builds {:main {:jar true
                               :source-path "src-cljs"
                               :compiler {:output-to "resources/public/js/cljs.js"
