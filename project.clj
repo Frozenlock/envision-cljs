6a59b6b05c7792214f69a34b540fbeea5e225e17
Frozenlock <frozenlock@gmail.com>
2013-01-28 02:53:01 -0500

    closure-js, not clojure-js...

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.frozenlock/envision-cljs "0.1.8"
+(defproject org.clojars.frozenlock/envision-cljs "0.1.9"
   :description "Wrapper for the Envision.js library (Graphs!)"
   :url "https://github.com/Frozenlock/envision-cljs"
   :license {:name "Eclipse Public License"
